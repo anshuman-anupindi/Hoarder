23cab203c3f82d3db64f505ab49a590e1c9c5a8a
Anshuman Anupindi <anshuman.anupindi@gmail.com>
2021-06-09 19:24:09 +1000

    not working

--- main.rb
+++ main.rb
@@ -26,6 +26,8 @@ get '/' do
   erb :index
 end
 
+# PATCH CRUD
+
 get '/patches' do
   sql_patches = "SELECT * FROM patches;" # WHERE user_id = #{session[:user_id]}
   patches = run_sql(sql_patches)
@@ -33,7 +35,7 @@ get '/patches' do
 
   patches.each do |patch|
     sql_themes = "SELECT * FROM themes WHERE theme_id = $1;"
-    themes.push(run_sql(sql_themes, [patch['theme_id']])[0]) 
+    themes.push(run_sql(sql_themes, [patch['theme_id']])[0])
     # sql_themes = "SELECT * FROM themes WHERE (theme_id = $1 AND user_id = $2);"
     # themes.push(run_sql(sql_themes, [patch['theme_id'], session[:user_id]])[0])
   end
@@ -53,6 +55,8 @@ post '/patches' do
   theme_info = [params['theme_name'], params['bg_img_url'], params['main_img_url']]
   run_sql(sql_themes, theme_info)
 
+  # inserting is fine
+
   sql_theme_id = "SELECT theme_id FROM themes WHERE (theme_name = $1 AND bg_img_url = $2 AND main_img_url = $3);"
   theme_id = run_sql(sql_theme_id, theme_info)[0]['theme_id']
 
@@ -65,7 +69,7 @@ end
 
 # view patchwork/ list of patches
 
-get '/patches/:id' do
+get '/patches/:patch_id' do
   patch_id = [params['patch_id']]
   res = run_sql("SELECT * FROM patches WHERE patch_id = $1;", patch_id)
   patch = res[0]
@@ -77,29 +81,59 @@ end
 
 # edit patch form
 
-get '/patches/:id/edit' do
+get '/patches/:patch_id/edit' do
   patch_id = [params['patch_id']]
   res = run_sql("SELECT * FROM patches WHERE patch_id = $1;", patch_id)
   patch = res[0]
-  erb :edit_patch_form, locals: {patch: patch}
+
+  songs = run_sql("SELECT * FROM songs WHERE patch_id = $1;", patch_id)
+  themes = run_sql("SELECT * FROM themes;")
+  erb :edit_patch_form, locals: {patch: patch, songs: songs, themes: themes}
 end
 
 # update patch
 
-put '/patches/:id' do
-  sql = "UPDATE patches SET patch_name = $1, patch_desc = $2, theme_id = $3;"
+put '/patches/:patch_id' do
+  sql_patch = "UPDATE patches SET patch_name = $1, patch_desc = $2, theme_id = $3;"
   patch_info = [params['patch_name'], params['patch_desc'], params['theme_id']]
-  run_sql(sql, patch_info)
-  redirect '/patches'
+  run_sql(sql_patch, patch_info)
+  
+  patch_id = params['patch_id']
+
+  redirect "/patches/#{patch_id}"
 end
 
 # delete a patch
 
-delete '/patches/:id' do
-  patch_id = [params['id']]
+delete '/patches/:patch_id' do
+  patch_id = [params['patch_id']]
   sql = "DELETE FROM patches WHERE patch_id = $1"
   run_sql(sql, patch_id)
+  redirect '/patches'
 end
+
+# SONG CRUD
+
+get '/patches/:id/songs/new' do
+  erb :new_song_form
+end
+
+post '/patches/:patch_id/songs' do
+  redirect "/patches/#{params['patch_id']}"
+end
+
+get '/songs/:id/edit' do
+  erb :edit_song_form, locals: {song_id: params['id']}
+end
+
+put '/songs/:id' do
+end
+
+# THEME CRUD
+
+
+
+
 
 # login page
 
